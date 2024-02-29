4d56a099449fc826e637b533653a97b7e56d454b
armytle <161580724+armytle@users.noreply.github.com>
2024-02-29 22:31:38 +0700

    Update script.sql

--- script.sql
+++ script.sql
@@ -4,12 +4,44 @@ GO;
 USE ASGMT01;
 GO;
 
-CREATE TABLE Books (
-    id INT,
-    title VARCHAR(255),
-    author VARCHAR(255),
-    PRIMARY KEY (id)
+CREATE TABLE BookCategories (
+    CategoryID INT PRIMARY KEY,
+    CategoryName NVARCHAR(255)
 );
-GO;
-
-SELECT * FROM Books;
+CREATE TABLE Author (
+    AuthorID INT PRIMARY KEY,
+    Author_name NVARCHAR(100)
+);
+CREATE TABLE Books (
+    BookID INT PRIMARY KEY,
+    Title NVARCHAR(255),
+    AuthorID INT,
+    Price DECIMAL(10, 2),
+    QuantityInStock INT,
+    OtherDetails NVARCHAR(MAX),
+    CategoryID INT,
+    Introduce NVARCHAR(MAX),
+    Publishinghouse NVARCHAR(255),
+    Publisher NVARCHAR(255),
+    Year INT,
+    Size NVARCHAR(50),
+    Weight DECIMAL(10, 2),
+    FOREIGN KEY (AuthorID) REFERENCES Author(AuthorID),
+    FOREIGN KEY (CategoryID) REFERENCES BookCategories(CategoryID)
+);
+CREATE TABLE Customer (
+    CustomerID INT PRIMARY KEY IDENTITY,
+    Last_name VARCHAR(100),
+    First_name VARCHAR(100),
+    Phone VARCHAR(20) UNIQUE,
+    Email VARCHAR(255) UNIQUE,
+    Address NVARCHAR(255)
+);
+CREATE TABLE Admin (
+    AdminID INT PRIMARY KEY,
+    Last_name VARCHAR(100),
+    First_name VARCHAR(100),
+    DateOfBirth DATE,
+    Password VARCHAR(255),
+    Phone VARCHAR(20)
+);
